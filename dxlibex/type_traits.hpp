9c9ed211b0b2aea0c8bd4e8561360f20cc403428
Nameless <Nagarei@users.noreply.github.com>
2016-01-07 22:37:20 +0900

    #28 type_traits.hppを追加

---
+++ dxlibex/type_traits.hpp
@@ -0,0 +1,15 @@
+/*=============================================================================
+  Copyright (C) 2015-2016 DxLibEx project
+  https://github.com/Nagarei/DxLibEx/
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See http://www.boost.org/LICENSE_1_0.txt)
+=============================================================================*/
+#ifndef DXLE_INC_TYPE_TRAITS_HPP_
+#define DXLE_INC_TYPE_TRAITS_HPP_
+
+#include "dxlibex/type_traits/enable_if.hpp"
+#include "dxlibex/type_traits/first_enabled.hpp"
+#include "dxlibex/type_traits/ignore.hpp"
+
+#endif//#ifndef DXLE_INC_TYPE_TRAITS_HPP_
