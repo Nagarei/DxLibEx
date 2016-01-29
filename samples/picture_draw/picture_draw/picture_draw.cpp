75eea67ee7707a244fb0a37a560edd4f591eb68b
yumetodo <yume-wikijp@live.jp>
2016-01-30 00:55:06 +0900

    以下のcommitの修正にsampleを対応させる 166f2464948639822aa65f95aa4c818dcbd61a88 ff121454c17b08e55e569e502ce6a346ab942bad

--- samples/picture_draw/picture_draw/picture_draw.cpp
+++ samples/picture_draw/picture_draw/picture_draw.cpp
@@ -16,7 +16,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	SetDrawScreen(DX_SCREEN_BACK);
 	// BMP画像のメモリへの読みこみ
 	//添付の絵素材は https://www.pakutaso.com/20150142021post-5083.html より
-	auto GHandle = dxle::Graph2D::LoadGraph("bsHIRO92_tukinomieruoka.jpg");//Texture2D型
+	auto GHandle = dxle::graph2d::LoadGraph("bsHIRO92_tukinomieruoka.jpg");//Texture2D型
 	GHandle.DrawGraph(0, 0, false);// 画面左上に描画します(『DrawGraph』を使用)
 	const auto font_h = CreateFontToHandle(nullptr, 12, 2, DX_FONTTYPE_ANTIALIASING);//フォントハンドルの作成
 	DrawStringToHandle(20, 20, GHandle.GetGraphFilePath().c_str(), GetColor(250, 250, 0), font_h);//画像のフルパスを表示
