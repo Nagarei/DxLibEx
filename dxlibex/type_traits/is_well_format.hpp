5f5cda7196dfc8e263d4a75dea6a04f8c572ae36
yumetodo <yume-wikijp@live.jp>
2016-02-07 14:51:41 +0900

    コンパクトな記述に

--- dxlibex/type_traits/is_well_format.hpp
+++ dxlibex/type_traits/is_well_format.hpp
@@ -8,22 +8,23 @@
 #ifndef DXLE_INC_TYPE_TRAITS_IS_WELL_FORMAT_HPP_
 #define DXLE_INC_TYPE_TRAITS_IS_WELL_FORMAT_HPP_
 #include <type_traits>
+#include "enable_if.hpp"
 namespace dxle {
 //!inline
 namespace type_traits {
 
-	template<typename T, typename = std::nullptr_t>
+	template<typename T, typename = nullptr_t>
 	struct has_operator_notequal_to_zero_impl : std::false_type {};
 	template<typename T>
-	struct has_operator_notequal_to_zero_impl < T, typename enable_if<ignore_type<decltype(std::declval<T>() != 0)>::value, std::nullptr_t>::type >
+	struct has_operator_notequal_to_zero_impl < T, enable_if_t<ignore_type<decltype(std::declval<T>() != 0)>::value, nullptr_t>>
 		: std::true_type {};
 	template<typename T>
 	struct has_operator_notequal_to_zero : has_operator_notequal_to_zero_impl<T> {};
 	
-	template<typename T, typename = std::nullptr_t>
+	template<typename T, typename = nullptr_t>
 	struct has_operator_notequal_to_this_impl : std::false_type {};
 	template<typename T>
-	struct has_operator_notequal_to_this_impl < T, typename enable_if<ignore_type<decltype(std::declval<T>() != std::declval<T>())>::value, std::nullptr_t>::type >
+	struct has_operator_notequal_to_this_impl < T, enable_if_t<ignore_type<decltype(std::declval<T>() != std::declval<T>())>::value, nullptr_t>>
 		: std::true_type {};
 	template<typename T>
 	struct has_operator_notequal_to_this : has_operator_notequal_to_this_impl<T> {};
