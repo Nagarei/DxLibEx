76952a7cf7f7e83efb10f7acfec44f36614c4285
yumetodo <yume-wikijp@live.jp>
2016-02-26 15:49:51 +0900

    #42 命名規則変更

--- dxlibex/Sound.h
+++ dxlibex/Sound.h
@@ -31,6 +31,38 @@ namespace dxle
 	namespace sound
 	{
 		namespace ch = std::chrono;
+		enum class reverb : int {
+			default          = DX_REVERB_PRESET_DEFAULT,// デフォルト
+			generic          = DX_REVERB_PRESET_GENERIC,// 一般的な空間
+			padded_cell      = DX_REVERB_PRESET_PADDEDCELL,// 精神病患者室(？)
+			room             = DX_REVERB_PRESET_ROOM,// 部屋
+			bathroom         = DX_REVERB_PRESET_BATHROOM,// バスルーム
+			living_room      = DX_REVERB_PRESET_LIVINGROOM,// リビングルーム
+			stone_room       = DX_REVERB_PRESET_STONEROOM,// 石の部屋
+			auditorium       = DX_REVERB_PRESET_AUDITORIUM,// 講堂
+			consert_hall     = DX_REVERB_PRESET_CONCERTHALL,// コンサートホール
+			cave             = DX_REVERB_PRESET_CAVE,// 洞穴
+			arena            = DX_REVERB_PRESET_ARENA,// 舞台
+			hangar           = DX_REVERB_PRESET_HANGAR,// 格納庫
+			carpeted_hallway = DX_REVERB_PRESET_CARPETEDHALLWAY,// カーペットが敷かれた玄関
+			hallway          = DX_REVERB_PRESET_HALLWAY,// 玄関
+			stone_corridor   = DX_REVERB_PRESET_STONECORRIDOR,// 石の廊下
+			alley            = DX_REVERB_PRESET_ALLEY,// 裏通り
+			forest           = DX_REVERB_PRESET_FOREST,// 森
+			city             = DX_REVERB_PRESET_CITY,// 都市
+			mountains        = DX_REVERB_PRESET_MOUNTAINS,// 山
+			quarry           = DX_REVERB_PRESET_QUARRY,// 採石場
+			plain            = DX_REVERB_PRESET_PLAIN,// 平原
+			parking_lot      = DX_REVERB_PRESET_PARKINGLOT,// 駐車場
+			sewer_pipe       = DX_REVERB_PRESET_SEWERPIPE,// 下水管
+			under_water      = DX_REVERB_PRESET_UNDERWATER,// 水面下
+			small_room       = DX_REVERB_PRESET_SMALLROOM,// 小部屋
+			medium_room      = DX_REVERB_PRESET_MEDIUMROOM,// 中部屋
+			large_room       = DX_REVERB_PRESET_LARGEROOM,// 大部屋
+			megium_hall      = DX_REVERB_PRESET_MEDIUMHALL,// 中ホール
+			large_hall       = DX_REVERB_PRESET_LARGEHALL,// 大ホール
+			plate            = DX_REVERB_PRESET_PLATE// 板
+		};
 		class sound final : public impl::Unique_HandledObject_Bace < sound >
 		{
 		public:
@@ -40,11 +72,11 @@ namespace dxle
 
 			sound& operator=(const sound& other) = delete;
 			sound& operator=(sound&& other) DXLE_NOEXCEPT_OR_NOTHROW{ Unique_HandledObject_Bace<sound>::operator=(std::move(other)); return *this; }
-				
+
 			//!\~japanese サウンドを削除する
 			//!\~english  Delete this sound data
 			inline void delete_this(bool LogOutFlag = false) { DxLib::DeleteSoundMem(GetHandle(), LogOutFlag); }
-			
+
 			//! 同じサウンドデータを使用するサウンドハンドルを作成する( DX_SOUNDDATATYPE_MEMNOPRESS タイプのサウンドハンドルのみ可能 )
 			sound DuplicateSoundMem(int BufferNum = 3)const DXLE_NOEXCEPT_OR_NOTHROW{ return DxLib::DuplicateSoundMem(GetHandle(), BufferNum); }
 
@@ -69,66 +101,62 @@ namespace dxle
 			int	DeleteSoundMem(int LogOutFlag = FALSE) { return DxLib::DeleteSoundMem(GetHandle(), LogOutFlag); }
 
 			//操作
-			int			play(int PlayType, int TopPositionFlag = TRUE);								// サウンドハンドルを再生する
-			int			stop();						// サウンドハンドルの再生を停止する
-			int			is_playing();						// サウンドハンドルが再生中かどうかを取得する
-			int			SetPan(int PanPal);						// サウンドハンドルのパンを設定する( 100分の1デシベル単位 0 ～ 10000 )
-			int			ChangePan(int16_t PanPal);						// サウンドハンドルのパンを設定する( -255 ～ 255 )
-			int			GetPan();						// サウンドハンドルのパンを取得する
-			int			SetVolume(int VolumePal);						// サウンドハンドルのボリュームを設定する( 100分の1デシベル単位 0 ～ 10000 )
-			int			ChangeVolume(uint8_t VolumePal);						// サウンドハンドルのボリュームを設定する( 0 ～ 255 )
-			int			GetVolume();						// サウンドハンドルのボリュームを取得する
-			int			SetChannelVolume(int Channel, int VolumePal);						// サウンドハンドルの指定のチャンネルのボリュームを設定する( 100分の1デシベル単位 0 ～ 10000 )
-			int			ChangeChannelVolume(int Channel, uint8_t VolumePal);						// サウンドハンドルの指定のチャンネルのボリュームを設定する( 0 ～ 255 )
-			int			GetChannelVolume(int Channel);						// サウンドハンドルの指定のチャンネルのボリュームを取得する
-			int			SetFrequency(int FrequencyPal);						// サウンドハンドルの再生周波数を設定する
-			int			GetFrequency();						// サウンドハンドルの再生周波数を取得する
-			int			ResetFrequency();						// サウンドハンドルの再生周波数を読み込み直後の状態に戻す
+			int play(int PlayType, int TopPositionFlag = TRUE);								// サウンドハンドルを再生する
+			int stop();						// サウンドハンドルの再生を停止する
+			int is_playing();						// サウンドハンドルが再生中かどうかを取得する
+			int set_pan(int PanPal);						// サウンドハンドルのパンを設定する( 100分の1デシベル単位 0 ～ 10000 )
+			int change_pan(int16_t PanPal);						// サウンドハンドルのパンを設定する( -255 ～ 255 )
+			int get_pan();						// サウンドハンドルのパンを取得する
+			int set_volume(int VolumePal);						// サウンドハンドルのボリュームを設定する( 100分の1デシベル単位 0 ～ 10000 )
+			int change_volume(uint8_t VolumePal);						// サウンドハンドルのボリュームを設定する( 0 ～ 255 )
+			int get_volume();						// サウンドハンドルのボリュームを取得する
+			int set_volume(int Channel, int VolumePal);						// サウンドハンドルの指定のチャンネルのボリュームを設定する( 100分の1デシベル単位 0 ～ 10000 )
+			int change_volume(int Channel, uint8_t VolumePal);						// サウンドハンドルの指定のチャンネルのボリュームを設定する( 0 ～ 255 )
+			int get_volume(int Channel);						// サウンドハンドルの指定のチャンネルのボリュームを取得する
+			int SetFrequency(int FrequencyPal);						// サウンドハンドルの再生周波数を設定する
+			int GetFrequency();						// サウンドハンドルの再生周波数を取得する
+			int ResetFrequency();						// サウンドハンドルの再生周波数を読み込み直後の状態に戻す
 
-			int			SetNextPlayPan(int PanPal);						// サウンドハンドルの次の再生にのみ使用するパンを設定する( 100分の1デシベル単位 0 ～ 10000 )
-			int			ChangeNextPlayPan(int16_t PanPal);						// サウンドハンドルの次の再生にのみ使用するパンを設定する( -255 ～ 255 )
-			int			SetNextPlayVolume(int VolumePal);						// サウンドハンドルの次の再生にのみ使用するボリュームを設定する( 100分の1デシベル単位 0 ～ 10000 )
-			int			ChangeNextPlayVolume(uint8_t VolumePal);						// サウンドハンドルの次の再生にのみ使用するボリュームを設定する( 0 ～ 255 )
-			int			SetNextPlayChannelVolume(int Channel, int VolumePal);						// サウンドハンドルの次の再生にのみ使用するチャンネルのボリュームを設定する( 100分の1デシベル単位 0 ～ 10000 )
-			int			ChangeNextPlayChannelVolume(int Channel, uint8_t VolumePal);						// サウンドハンドルの次の再生にのみ使用するチャンネルのボリュームを設定する( 0 ～ 255 )
-			int			SetNextPlayFrequency(int FrequencyPal);						// サウンドハンドルの次の再生にのみ使用する再生周波数を設定する
+			int set_next_play_pan(int PanPal);						// サウンドハンドルの次の再生にのみ使用するパンを設定する( 100分の1デシベル単位 0 ～ 10000 )
+			int change_next_play_pan(int16_t PanPal);						// サウンドハンドルの次の再生にのみ使用するパンを設定する( -255 ～ 255 )
+			int set_next_play_volume(int VolumePal);						// サウンドハンドルの次の再生にのみ使用するボリュームを設定する( 100分の1デシベル単位 0 ～ 10000 )
+			int cange_next_play_volume(uint8_t VolumePal);						// サウンドハンドルの次の再生にのみ使用するボリュームを設定する( 0 ～ 255 )
+			int set_next_play_volume(int Channel, int VolumePal);						// サウンドハンドルの次の再生にのみ使用するチャンネルのボリュームを設定する( 100分の1デシベル単位 0 ～ 10000 )
+			int cange_next_play_volume(int Channel, uint8_t VolumePal);						// サウンドハンドルの次の再生にのみ使用するチャンネルのボリュームを設定する( 0 ～ 255 )
+			int set_next_play_frequency(int FrequencyPal);						// サウンドハンドルの次の再生にのみ使用する再生周波数を設定する
 
-			int			SetCurrentPosition(int SamplePosition);						// サウンドハンドルの再生位置をサンプル単位で設定する(再生が止まっている時のみ有効)
-			int			GetCurrentPosition();						// サウンドハンドルの再生位置をサンプル単位で取得する
-			int			SetSoundCurrentPosition(int Byte);						// サウンドハンドルの再生位置をバイト単位で設定する(再生が止まっている時のみ有効)
-			int			GetSoundCurrentPosition();						// サウンドハンドルの再生位置をバイト単位で取得する
-			int			SetSoundCurrentTime(ch::milliseconds Time);						// サウンドハンドルの再生位置をミリ秒単位で設定する(圧縮形式の場合は正しく設定されない場合がある)
-			ch::milliseconds GetSoundCurrentTime();						// サウンドハンドルの再生位置をミリ秒単位で取得する(圧縮形式の場合は正しい値が返ってこない場合がある)
-			int			GetSoundTotalSample();						// サウンドハンドルの音の総時間をサンプル単位で取得する
-			ch::milliseconds GetSoundTotalTime();						// サウンドハンドルの音の総時間をミリ秒単位で取得する
+			typedef int sample_n_t;//! for sample num
+			int set_current_position(sample_n_t SamplePosition);						// サウンドハンドルの再生位置をサンプル単位で設定する(再生が止まっている時のみ有効)
+			sample_n_t get_current_position();						// サウンドハンドルの再生位置をサンプル単位で取得する
+			int set_sound_current_position(int Byte);						// サウンドハンドルの再生位置をバイト単位で設定する(再生が止まっている時のみ有効)
+			int get_sound_currentPosition();						// サウンドハンドルの再生位置をバイト単位で取得する
+			int set_sound_current_time(ch::milliseconds Time);						// サウンドハンドルの再生位置をミリ秒単位で設定する(圧縮形式の場合は正しく設定されない場合がある)
+			ch::milliseconds get_sound_current_time();						// サウンドハンドルの再生位置をミリ秒単位で取得する(圧縮形式の場合は正しい値が返ってこない場合がある)
+			sample_n_t get_sound_total_sample();						// サウンドハンドルの音の総時間をサンプル単位で取得する
+			ch::milliseconds get_sound_total_time();						// サウンドハンドルの音の総時間をミリ秒単位で取得する
 
-			int			SetLoopPos(ch::milliseconds LoopTime);						// SetLoopTimePos の別名関数
-			int			SetLoopTimePos(ch::milliseconds LoopTime);						// サウンドハンドルにループ位置を設定する(ミリ秒単位)
-			int			SetLoopSamplePos(int LoopSamplePosition);						// サウンドハンドルにループ位置を設定する(サンプル単位)
+			int set_loop_time_pos(ch::milliseconds LoopTime);						// サウンドハンドルにループ位置を設定する(ミリ秒単位)
+			int set_loop_sample_pos(sample_n_t LoopSamplePosition);						// サウンドハンドルにループ位置を設定する(サンプル単位)
 
-			int			SetLoopStartTimePos(ch::milliseconds LoopStartTime);						// サウンドハンドルにループ開始位置を設定する(ミリ秒単位)
-			int			SetLoopStartSamplePos(int LoopStartSamplePosition);						// サウンドハンドルにループ開始位置を設定する(サンプル単位)
+			int set_loop_start_time_pos(ch::milliseconds LoopStartTime);						// サウンドハンドルにループ開始位置を設定する(ミリ秒単位)
+			int set_loop_start_sample_pos(sample_n_t LoopStartSamplePosition);						// サウンドハンドルにループ開始位置を設定する(サンプル単位)
 
-			int			SetPlayFinishDelete(int DeleteFlag);						// サウンドハンドルの再生が終了したら自動的にハンドルを削除するかどうかを設定する
+			int register_delete_play_finish(bool DeleteFlag = true) { return DxLib::SetPlayFinishDeleteSoundMem(DeleteFlag, GetHandle()); }						// サウンドハンドルの再生が終了したら自動的にハンドルを削除するかどうかを設定する
 
-			int			Set3DReverbParam(const SOUND3D_REVERB_PARAM *Param);						// サウンドハンドルの３Ｄサウンド用のリバーブパラメータを設定する
-			int			Set3DPresetReverbParam(int PresetNo /* DX_REVERB_PRESET_DEFAULT 等 */);						// サウンドハンドルの３Ｄサウンド用のリバーブパラメータをプリセットを使用して設定する
-			int			Set3DReverbParamAll(const SOUND3D_REVERB_PARAM *Param, int PlaySoundOnly = FALSE);							// 全ての３Ｄサウンドのサウンドハンドルにリバーブパラメータを設定する( PlaySoundOnly TRUE:再生中のサウンドにのみ設定する  FALSE:再生していないサウンドにも設定する )
-			int			Set3DPresetReverbParamAll(int PresetNo /* DX_REVERB_PRESET_DEFAULT 等 */, int PlaySoundOnly = FALSE);			// 全ての３Ｄサウンドのサウンドハンドルにリバーブパラメータをプリセットを使用して設定する( PlaySoundOnly TRUE:再生中のサウンドにのみ設定する  FALSE:再生していないサウンドにも設定する )
-			int			Get3DReverbParam(SOUND3D_REVERB_PARAM *ParamBuffer);						// サウンドハンドルに設定されている３Ｄサウンド用のリバーブパラメータを取得する
-			int			Get3DPresetReverbParam(SOUND3D_REVERB_PARAM *ParamBuffer, int PresetNo /* DX_REVERB_PRESET_DEFAULT 等 */);		// プリセットの３Ｄサウンド用のリバーブパラメータを取得する
+			int set_3d_peverb_param(const SOUND3D_REVERB_PARAM& Param) { return DxLib::Set3DReverbParamSoundMem(&Param, GetHandle()); }						// サウンドハンドルの３Ｄサウンド用のリバーブパラメータを設定する
+			int set_3d_preset_reverb_param(reverb PresetNo);						// サウンドハンドルの３Ｄサウンド用のリバーブパラメータをプリセットを使用して設定する
+			SOUND3D_REVERB_PARAM get_3d_reverb_param();						// サウンドハンドルに設定されている３Ｄサウンド用のリバーブパラメータを取得する
 
-			int			Set3DPosition(VECTOR Position);						// サウンドハンドルの３Ｄサウンド用の再生位置を設定する
-			int			Set3DPosition(const point3df& Position);						// サウンドハンドルの３Ｄサウンド用の再生位置を設定する
-			int			Set3DRadius(float Radius);						// サウンドハンドルの３Ｄサウンド用の音が聞こえる距離を設定する
-			int			Set3DVelocity(VECTOR Velocity);						// サウンドハンドルの３Ｄサウンド用の移動速度を設定する
-			int			Set3DVelocity(const point3df& Velocity);						// サウンドハンドルの３Ｄサウンド用の移動速度を設定する
-
-			int			SetNextPlay3DPosition(VECTOR Position);						// サウンドハンドルの次の再生のみに使用する３Ｄサウンド用の再生位置を設定する
-			int			SetNextPlay3DPosition(const point3df& Position);						// サウンドハンドルの次の再生のみに使用する３Ｄサウンド用の再生位置を設定する
-			int			SetNextPlay3DRadius(float Radius);						// サウンドハンドルの次の再生のみに使用する３Ｄサウンド用の音が聞こえる距離を設定する
-			int			SetNextPlay3DVelocity(VECTOR Velocity);						// サウンドハンドルの次の再生のみに使用する３Ｄサウンド用の移動速度を設定する
-			int			SetNextPlay3DVelocity(const point3df& Velocity);						// サウンドハンドルの次の再生のみに使用する３Ｄサウンド用の移動速度を設定する
+			int set_3d_position(VECTOR Position);						// サウンドハンドルの３Ｄサウンド用の再生位置を設定する
+			int set_3d_position(const point3df& Position);						// サウンドハンドルの３Ｄサウンド用の再生位置を設定する
+			int set_3d_radius(float Radius);						// サウンドハンドルの３Ｄサウンド用の音が聞こえる距離を設定する
+			int set_3d_velocity(VECTOR Velocity);						// サウンドハンドルの３Ｄサウンド用の移動速度を設定する
+			int set_3d_velocity(const point3df& Velocity);						// サウンドハンドルの３Ｄサウンド用の移動速度を設定する
+			int set_next_play_3d_position(VECTOR Position);						// サウンドハンドルの次の再生のみに使用する３Ｄサウンド用の再生位置を設定する
+			int set_next_play_3d_position(const point3df& Position);						// サウンドハンドルの次の再生のみに使用する３Ｄサウンド用の再生位置を設定する
+			int set_next_play_3d_rdius(float Radius);						// サウンドハンドルの次の再生のみに使用する３Ｄサウンド用の音が聞こえる距離を設定する
+			int set_next_play_3d_velocity(VECTOR Velocity);						// サウンドハンドルの次の再生のみに使用する３Ｄサウンド用の移動速度を設定する
+			int set_next_play_3d_velocity(const point3df& Velocity);						// サウンドハンドルの次の再生のみに使用する３Ｄサウンド用の移動速度を設定する
 
 		private:
 			sound(int param_handle) DXLE_NOEXCEPT_OR_NOTHROW : Unique_HandledObject_Bace(param_handle){}
