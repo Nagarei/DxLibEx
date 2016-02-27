e90de53683679f1a8e0d25f5c9c1e84ecaa54f8f
Nameless <Nagarei@users.noreply.github.com>
2016-02-27 15:40:23 +0900

    #49 htmlの生成と削除を分離

--- html/make_html.bat
+++ html/make_html.bat
@@ -1,5 +1,3 @@
-rd /s /q ..\DxLibEx_Web\en\html
-rd /s /q ..\DxLibEx_Web\ja\html
 if defined DOXYGEN_PATH (
 	"%DOXYGEN_PATH%\doxygen.exe" Doxyfile_ja.doxy
 	"%DOXYGEN_PATH%\doxygen.exe" Doxyfile_en.doxy
