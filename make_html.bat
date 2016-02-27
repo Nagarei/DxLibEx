a8ec0b4316e053020dd0db317fac105631cad88f
Nameless <Nagarei@users.noreply.github.com>
2016-02-27 14:46:20 +0900

    #49 生成用batファイルに前のデータの削除命令を追加

--- make_html.bat
+++ make_html.bat
@@ -1,2 +1,9 @@
-doxygen.exe Doxyfile_ja.doxy
-doxygen.exe Doxyfile_en.doxy
+rd /s /q ..\DxLibEx_Web\en\html
+rd /s /q ..\DxLibEx_Web\ja\html
+if defined DOXYGEN_PATH (
+	"%DOXYGEN_PATH%\doxygen.exe" Doxyfile_ja.doxy
+	"%DOXYGEN_PATH%\doxygen.exe" Doxyfile_en.doxy
+) else (
+	doxygen.exe Doxyfile_ja.doxy
+	doxygen.exe Doxyfile_en.doxy
+)
