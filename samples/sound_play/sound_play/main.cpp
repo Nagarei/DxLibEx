c30b32b7c778ca26c87224c2e21df3b6e0637fb7
yumetodo <yume-wikijp@live.jp>
2016-03-14 23:55:07 +0900

    VSでUDLsが動かないのはnamespaceで囲った後using namespaceしたからではないかと疑い、グローバル名前空間で定義するも失敗する

--- samples/sound_play/sound_play/main.cpp
+++ samples/sound_play/sound_play/main.cpp
@@ -6,4 +6,5 @@ int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPSTR /
 	constexpr auto rate = 300_dB;
 	constexpr auto rate2 = 4000_myrioB;
 	constexpr auto rate3 = dxle::bel_cast<dxle::bel>(rate);
+	constexpr dxle::deci_bel rate4(100);
 }
