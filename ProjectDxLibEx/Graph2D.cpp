cd6c712c63a1664486e10932c1c974b990a3b722
soukouki <soukouki0@yahoo.co.jp>
2016-05-09 15:44:37 +0900

    includeエラーが出ていたのを修正

--- ProjectDxLibEx/Graph2D.cpp
+++ ProjectDxLibEx/Graph2D.cpp
@@ -5,7 +5,7 @@
   Distributed under the Boost Software License, Version 1.0.
   (See http://www.boost.org/LICENSE_1_0.txt)
 =============================================================================*/
-#include "dxlibex/graph2d.h"
+#include "dxlibex/graph2d.hpp"
 
 namespace dxle{
 namespace Graph2D {
