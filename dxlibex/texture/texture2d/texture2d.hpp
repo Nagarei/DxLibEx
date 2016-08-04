e57e750dfeb1f112c97faa954da79811bb6447b5
yumetodo <yume-wikijp@live.jp>
2016-08-05 03:03:44 +0900

    fix #84 : define CINTERFACE before include Windwos.h or DxLib.h

--- dxlibex/texture/texture2d/texture2d.hpp
+++ dxlibex/texture/texture2d/texture2d.hpp
@@ -9,6 +9,12 @@
 #define DXLE_INC_TEXTURE_TEXTURE2D_TEXTURE2D_HPP_
 
 #include "dxlibex/config/no_min_max.h"
+#if !defined(CINTERFACE) && defined(__c2__) &&  __clang_major__ == 3 && __clang_minor__ == 8
+//To avoid compile error
+//C:\Program Files (x86)\Windows Kits\8.1\Include\um\combaseapi.h(229,21): error : unknown type name 'IUnknown'
+//          static_cast<IUnknown*>(*pp);    // make sure everyone derives from IUnknown
+#define CINTERFACE
+#endif
 #include "DxLib.h"
 #include <cstdint>
 #include "../texture2d.hpp"
